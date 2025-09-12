763885d9dac881a1966312d6319a6f0d7c42c170
UDAYKOTESWARARAOCHAKRAPU <udaykoteswarao@gmail.com>
2025-09-12 10:17:49 +0530

    frontend setup

---
+++ Frontend/src/app/Pages/add-document/add-document.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-document',
+  imports: [],
+  templateUrl: './add-document.component.html',
+  styleUrl: './add-document.component.css'
+})
+export class AddDocumentComponent {
+
+}
--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent:() =>{
+            return import('./Pages/home/home.component').then(m => m.HomeComponent)
+        }
+    },
+    {
+        path: 'landing',
+        loadComponent:() =>{
+            return import('./Pages/landing/landing.component').then(m => m.LandingComponent)
+        }
+    },
+];
